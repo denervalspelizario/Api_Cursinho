3313182d67dae1f47770a0deee8a99711fc6c379
Denerval s Pelizario <denervalspelizario@gmail.com>
2024-04-30 19:58:28 -0300

    Api_Cursinho : criacao de estrutura base e instalacao de pacotes base

---
+++ Api_Cursinho/Api_Cursinho/Documentacao/Documentacao.cs
@@ -0,0 +1,9 @@
+﻿/*
+   PASSO A PASSO CRIACAO E DESENVOLVIMENTO API CURSINHO
+ 
+   [1] Criei o banco de dados direto no postgres e as tabelas
+ 
+   [2] Criacao dos Models que vão linkar com as tabelas criadas la no postgres
+ 
+ 
+ */
---
+++ Api_Cursinho/Api_Cursinho/Model/Administradores.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Api_Cursinho.Model
+{
+    [Table("administradores")] // linkando com tabela administradores
+    public class Administradores
+    {
+    }
+}
